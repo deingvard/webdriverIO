4cef8da05d6fa348972e1cb02ba49dae56d9f7ae
ihar <deingvard@gmail.com>
2020-12-15 17:24:03 +0300

    Initial commit

---
+++ Gruntfile.js
@@ -0,0 +1,17 @@
+module.exports = function(grunt) {
+
+    grunt.initConfig({
+        pkg: grunt.file.readJSON('package.json'),
+        webdriver: {
+            'test-local': {
+                configFile: './test/config/wdio.local.conf.js'
+            },
+        },
+    });
+
+    grunt.loadNpmTasks('grunt-mocha');
+    grunt.loadNpmTasks('grunt-webdriver');
+    grunt.registerTask('default', ['webdriver:test-local']);
+    // grunt.registerTask('default', ['webdriver:test-sauce']);
+    // grunt.registerTask('default', ['test-browserstack']);
+};
---
+++ test/config/wdio.local.conf.js
@@ -0,0 +1,17 @@
+const { config } = require('./wdio.shared.conf')
+
+exports.config = {
+    ...config,
+    ...{
+      services: ['selenium-standalone'],
+      capabilities: [
+          {
+            maxInstances: 5,
+            browserName: 'chrome',
+            'goog:chromeOptions': {
+                //headless: true
+            }
+        },
+      ]
+    }
+}
